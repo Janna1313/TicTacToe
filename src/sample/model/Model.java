5716e2e5eb142b504b4db1b19d918ac4fdd49acd
Platinwing <csaeume@gmail.com>
2019-11-27 11:10:55 +0100

    started model work

--- src/sample/model/Model.java
+++ src/sample/model/Model.java
@@ -1,4 +1,20 @@
 package sample.model;
 
+import java.util.ArrayList;
+
 public class Model {
+    int size;
+    int[][] grid;
+
+    public boolean makeTurn(int x, int y, int player){
+        
+    }
+
+    private void setField(int x, int y, int player){
+
+    }
+
+    public void generateField(int size){
+        grid = new int[size][size];
+    }
 }
