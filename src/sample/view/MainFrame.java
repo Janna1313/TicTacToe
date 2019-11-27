548e6c784443d1b5ab62bfa74426fdeb83d23303
Zlimca <goldslimelp@gmail.com>
2019-11-27 10:09:59 +0100

    hsup

---
+++ src/sample/view/MainFrame.java
@@ -0,0 +1,23 @@
+package sample.view;
+
+public class GameScreen implements View {
+    @Override
+    public void showWinner(int who) {
+        
+    }
+
+    @Override
+    public void put(int pl, int typ) {
+
+    }
+
+    @Override
+    public void illegalMove(int pl) {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
