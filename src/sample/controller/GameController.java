9bd00c77e2a77f2006076ac917a097c4947d627a
Janna <frischesgras88@gmx.de>
2019-12-11 09:19:48 +0100

    interface implemented & switchplayer

--- src/sample/controller/GameController.java
+++ src/sample/controller/GameController.java
@@ -1,7 +1,31 @@
 package sample.controller;
 
-public class GameController {
+import sample.model.Model;
+import sample.view.*;
+
+public class GameController implements Controller {
+    private int active_player = 1;
+    private Model model;
+    private MainFrame view;
 
 
+    @Override
+    public void checkMove() {
 
+    }
+
+    @Override
+    public void switchPlayer() {
+        active_player = active_player % 2 + 1;
+    }
+
+    @Override
+    public void restart() {
+
+    }
+
+    @Override
+    public void startGame() {
+
+    }
 }
