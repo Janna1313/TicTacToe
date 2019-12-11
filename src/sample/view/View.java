ca80138ffc68ed223628290b95c0a9502f772c17
Zlimca <goldslimelp@gmail.com>
2019-12-11 09:54:21 +0100

    hsup

--- src/sample/view/View.java
+++ src/sample/view/View.java
@@ -2,6 +2,7 @@ package sample.view;
 
 interface View {
     void showWinner(int who);
+    void showScoreBoard(String board);
     void put(int pl, int typ);
     void illegalMove(int pl);
     void init();
